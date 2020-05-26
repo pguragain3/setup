1002477bbd2ac4ea245a2d00e293bea6f3dc07e7
Prabesh Guragain <pguragain3@gmail.com>
2020-05-26 13:36:18 +0545

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+# adding repository
+sudo apt-add-repository -y ppa:teejee2008/ppa
+sudo add-apt-repository -y ppa:lutris-team/lutris
+
+
+#install apps
+sudo apt install celluloid telegram-desktop gnome-tweaks gnome-system-tools virtualbox celluloid ubuntu-restricted-extras timeshift -y
+sudo apt install default-jre default-jdk psensor snapd flatpak gimp shotcut steam  gnome-software-plugin-flatpak github-desktop -y
+sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils -y
+
+sudo adduser pguragain3 kvm -y
+sudo adduser pguragain3 libvirt -y
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+sudo snap install code --classic -y
+sudo snap install android-studio --classic -y
+sudo snap install obs-studio -y
+sudo snap install apache-netbeans --classic -y
+sudo snap install eclipse --classic -y
+
+flatpak install flathub com.viber.Viber -y
+flatpak install flathub com.jetbrains.PhpStorm -y
+flatpak install flathub com.sublimetext.three -y
+flatpak install flathub io.atom.Atom￼content_copy -y
+flatpak install flathub com.discordapp.Discord -y
