8474629704ee658485a53986d2ed0718280fdf56
Prabesh Guragain <pguragain3@gmail.com>
2020-07-27 10:49:07 +0545

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo add-apt-repository -y ppa:lutris-team/lutris
 
 
 #install apps
-sudo apt install celluloid telegram-desktop gnome-tweaks gnome-system-tools virtualbox celluloid ubuntu-restricted-extras timeshift -y
+sudo apt install celluloid telegram-desktop gnome-tweaks gnome-system-tools virtualbox celluloid ubuntu-restricted-extras gnome-software timeshift -y
 sudo apt install openjdk-14-jdk psensor snapd flatpak gimp shotcut steam  gnome-software-plugin-flatpak -y
 sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils -y
 
@@ -16,7 +16,6 @@ sudo adduser pguragain3 libvirt
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-sudo snap install code --classic
 sudo snap install android-studio --classic
 
 flatpak install flathub com.viber.Viber -y
