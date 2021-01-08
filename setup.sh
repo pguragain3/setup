ad5e82c0e1b4b9b8a87ac591fc7a01f13dcfe769
Prabesh Guragain <pguragain3@gmail.com>
2021-01-08 13:27:14 +0545

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ sudo apt-add-repository -y ppa:teejee2008/ppa
 #install apps
 sudo apt install gnome-tweaks grub-customizer chrome-gnome-shell -y
 sudo apt install gnome-shell-extensions gnome-menus gir1.2-gmenu-3.0 -y
-sudo apt install celluloid telegram-desktop gnome-tweaks gnome-system-tools virtualbox celluloid ubuntu-restricted-extras gnome-software timeshift apt-transport-https curl -y
-sudo apt install openjdk-8-jdk psensor gimp openshot steam stacer htop tlp -y --fix-missing
+sudo apt install telegram-desktop gnome-tweaks gnome-system-tools virtualbox celluloid ubuntu-restricted-extras timeshift apt-transport-https curl -y
+sudo apt install openjdk-8-jdk psensor gimp openshot htop tlp -y --fix-missing
 sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils -y
 
 
@@ -18,6 +18,3 @@ sudo adduser pguragain3 libvirt
 sudo apt update
 sudo systemctl start tlp
 sudo systemctl enable tlp
-
-sudo snap install android-studio --classic
-
