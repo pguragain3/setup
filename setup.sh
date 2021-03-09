89f0297eebe3e154aaa065ad502d5caf3cccd829
Prabesh Guragain <pguragain3@gmail.com>
2021-03-09 19:11:04 +0545

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,18 +5,10 @@ sudo apt upgrade -y
 sudo apt-add-repository -y ppa:teejee2008/ppa
 
 #install apps
-sudo apt install gnome-tweaks chrome-gnome-shell -y
-sudo apt install gnome-shell-extensions gnome-menus gir1.2-gmenu-3.0 -y
-sudo apt install telegram-desktop gnome-tweaks gnome-system-tools virtualbox timeshift apt-transport-https curl -y
-sudo apt install openjdk-14-jdk psensor gimp kdenlive htop tlp -y --fix-missing
-sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients snapd bridge-utils -y
-
-
-sudo adduser pguragain3 kvm
-sudo adduser pguragain3 libvirt
+sudo apt install gnome-tweaks chrome-gnome-shell gnome-shell-extensions gnome-menus gir1.2-gmenu-3.0 telegram-desktop gnome-tweaks gnome-system-tools virtualbox timeshift apt-transport-https curl openjdk-11-jdk psensor gimp kdenlive htop tlp qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils clementine synaptic -y --fix-missing
+sudo adduser pg kvm
+sudo adduser pg libvirt
 
 sudo apt update
 sudo systemctl start tlp
 sudo systemctl enable tlp
-
-snap install vlc
