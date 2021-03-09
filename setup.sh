7bcdcbedf38caeb8d3dc42c666a238e2e44dd7a1
Prabesh Guragain <pguragain3@gmail.com>
2021-03-09 19:13:34 +0545

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt upgrade -y
 sudo apt-add-repository -y ppa:teejee2008/ppa
 
 #install apps
-sudo apt install gnome-tweaks chrome-gnome-shell gnome-shell-extensions gnome-menus gir1.2-gmenu-3.0 telegram-desktop gnome-tweaks gnome-system-tools virtualbox timeshift apt-transport-https curl openjdk-11-jdk psensor gimp kdenlive htop tlp qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils clementine synaptic -y --fix-missing
+sudo apt install gnome-tweaks chrome-gnome-shell gnome-shell-extensions gnome-menus gir1.2-gmenu-3.0 telegram-desktop gnome-tweaks gnome-system-tools virtualbox timeshift apt-transport-https curl openjdk-11-jdk psensor gimp kdenlive htop tlp qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils clementine synaptic zsh-syntax-highlighting php composer npm php-sqlite3 php-mysql php-zip php-curl php-xml -y --fix-missing
 sudo adduser pg kvm
 sudo adduser pg libvirt
 
